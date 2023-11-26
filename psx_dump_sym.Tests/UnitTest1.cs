ec70100bc0c7963ea2cc9f97d925d3f04d7705c9
aybe <aybe@users.noreply.github.com>
2023-11-26 10:56:30 +0100

    Upgrade to a data-driven test.

--- psx_dump_sym.Tests/UnitTest1.cs
+++ psx_dump_sym.Tests/UnitTest1.cs
@@ -3,13 +3,27 @@ namespace psx_dump_sym.Tests;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    public static IEnumerable<object[]> DumpSymFileData()
     {
-        const string path = @"C:\GitHub\Hi-Octane\MAIN.SYM";
+        var path = UnitTestDataUtility.GetFullPath(".SYM file list.txt");
 
+        var text = File.ReadAllLines(path);
+
+        foreach (var line in text)
+        {
+            if (File.Exists(line))
+            {
+                yield return new object[] { line };
+            }
+        }
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(DumpSymFileData), DynamicDataSourceType.Method)]
+    public void DumpSymFile(string path)
+    {
         using var stream = File.OpenRead(path);
-
+        
         SymbolUtility.Dump(stream);
     }
 }
---
+++ psx_dump_sym.Tests/UnitTestDataUtility.cs
@@ -0,0 +1,9 @@
+namespace psx_dump_sym.Tests;
+
+public static class UnitTestDataUtility // TODO move to library
+{
+    public static string GetFullPath(in string path)
+    {
+        return Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
+    }
+}
