17199865fed0324cd487676be80ccacc8b5a97ba
aybe <aybe@users.noreply.github.com>
2023-11-27 11:10:41 +0100

    Make these properties get-only.

--- psx_dump_sym/Symbol.cs
+++ psx_dump_sym/Symbol.cs
@@ -2,7 +2,7 @@ namespace psx_dump_sym;
 
 public sealed class Symbol(SymbolHeader header, SymbolRecord record)
 {
-    public SymbolHeader Header { get; set; } = header;
+    public SymbolHeader Header { get; } = header;
 
-    public SymbolRecord Record { get; set; } = record;
+    public SymbolRecord Record { get; } = record;
 }
