247cc31859a06e75812c7cc0d6d9d904ccbc73f8
aybe <aybe@users.noreply.github.com>
2023-11-30 05:12:52 +0100

    Push members to ISymbolFunction.

--- DUMPSYM/ISymbolFunction.cs
+++ DUMPSYM/ISymbolFunction.cs
@@ -2,4 +2,19 @@ namespace DUMPSYM;
 
 public interface ISymbolFunction
 {
+    ushort FramePointer { get; }
+
+    uint Size { get; }
+
+    ushort ReturnAddressRegister { get; }
+
+    uint Mask { get; }
+
+    int MaskOffset { get; }
+
+    uint Line { get; }
+
+    string File { get; }
+
+    string Name { get; }
 }
