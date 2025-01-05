1c2c4977359da76743e9e8415620a7ebcf0dfec4
aybe <aybe@users.noreply.github.com>
2025-01-05 20:07:44 +0100

    Add quick test

---
+++ Tests/Test1.cs
@@ -0,0 +1,31 @@
+﻿using DUMPSYM;
+
+namespace Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        const string sym = """C:\Files\GitHub\psx_mnd_sym\cmd\sym_dump\main.sym""";
+        const string txt = """C:\Files\GitHub\psx_mnd_sym\cmd\sym_dump\main.txt""";
+        using var stream = File.OpenRead(sym);
+        var file = SymbolFile.Dump(stream);
+        var expected = File.ReadAllText(txt);
+        var actual = file.ToString();
+
+        var line = 0;
+        using var sourceReader = new StringReader(expected);
+        using var targetReader = new StringReader(actual);
+
+        while (true)
+        {
+            var sourceLine = sourceReader.ReadLine();
+            var targetLine = targetReader.ReadLine();
+            if (sourceLine is null && targetLine is null) break;
+            Assert.AreEqual(sourceLine, targetLine, $"Line {line + 1}");
+            line++;
+        }
+    }
+}
