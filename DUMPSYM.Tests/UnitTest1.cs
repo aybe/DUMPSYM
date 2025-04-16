0f8ef26707d3a0adc86286109e7f7ef311371822
aybe <aybe@users.noreply.github.com>
2025-04-16 02:33:13 +0200

    Remove bunch of older tests

--- DUMPSYM.Tests/UnitTest1.cs
+++ DUMPSYM.Tests/UnitTest1.cs
@@ -3,39 +3,6 @@
 [TestClass]
 public sealed class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
-    {
-        var file = GetSample();
-
-        const string txt = """C:\Files\GitHub\psx_mnd_sym\cmd\sym_dump\main.txt""";
-
-        var expected = File.ReadAllText(txt);
-        var actual = file.ToString();
-
-        var line = 0;
-        using var sourceReader = new StringReader(expected);
-        using var targetReader = new StringReader(actual);
-
-        while (true)
-        {
-            var sourceLine = sourceReader.ReadLine();
-            var targetLine = targetReader.ReadLine();
-
-            if (sourceLine is null && targetLine is null)
-            {
-                break;
-            }
-
-            if (sourceLine != targetLine)
-            {
-                Assert.Fail($"\nLine {line + 1}\nSource: {sourceLine}\nTarget: {targetLine}");
-            }
-
-            line++;
-        }
-    }
-
     public static SymbolFile GetSample() // TODO more
     {
         const string pa = """C:\Files\GitHub\psx_mnd_sym\cmd\sym_dump\main.sym""";
--- DUMPSYM.Tests/UnitTestDataUtility.cs
+++
@@ -1,9 +0,0 @@
-namespace DUMPSYM.Tests;
-
-public static class UnitTestDataUtility // TODO move to library
-{
-    public static string GetFullPath(in string path)
-    {
-        return Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
-    }
-}
