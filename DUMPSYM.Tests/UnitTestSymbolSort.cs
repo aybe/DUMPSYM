39222d7d0151818ca04e39c6ff0e98ee85d6d9ef
aybe <aybe@users.noreply.github.com>
2025-04-20 10:55:56 +0200

    Upgrade topo test with data rows

--- DUMPSYM.Tests/UnitTestSymbolSort.cs
+++ DUMPSYM.Tests/UnitTestSymbolSort.cs
@@ -9,16 +9,62 @@ namespace DUMPSYM.Tests;
 [TestClass]
 public sealed class UnitTestSymbolSort : UnitTestBase
 {
-    [TestMethod]
-    public void TopologicalSort()
+    [TestMethod] // TODO dynamic data
+    // ReSharper disable StringLiteralTypo
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\AFFECT.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\ANGLE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\BUILDING.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\CAMERA.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\CONTROL.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\DISTANCE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\DRAW.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\EFFECT.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\ENGINE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\FLOATLIB.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\FORLIB.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\GAME.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\GENMAP.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\LEVEL.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MAIN.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MAP.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MAPWHO.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MEMCARD.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MOVE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\MUSIC.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\OBJECT.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\OBJECTS.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\OPTMENU.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\PACKET.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\PERSON.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\POWERUP.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\PSXHOST.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\PSXIO.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SCANNER.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SCREENS.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SEARCH.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SHOT.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SOUND.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SSPRITE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\STATS.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SUPER.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\SWITCH.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\TEXT.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\THING.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\TRACK.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\VEHCOLID.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\VEHICLE.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\WEAPON.C.json")]
+    [DataRow(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\WEATHER.C.json")]
+    public void TopologicalSort(string path)
     {
         // TODO try passing them in original order to preserve initial order, this will require symbol header maybe
-        var lists = UnitTestSplit222
-            .GetSymbolRegistry(@"C:\Files\GitHub\! PSX\DUMPSYM\TestData\AFFECT.C.json")
-            .CreateLists();
+        
+        var lists = UnitTestSplit222.GetSymbolRegistry(path).CreateLists();
+
         var symbols = lists
             .Select(s => new Sym { Code = s }).ToList();
 
+        // AFFECT.C
         symbols.AddRange([
             new Sym { Code = new Code(new SymbolRecordDef { Class = SymbolStorageClass.TPDEF, Type = new SymbolType { Value = 0 }, Name = "NULL" }) },
             new Sym { Code = new Code(new SymbolRecordDef { Class = SymbolStorageClass.TPDEF, Type = new SymbolType { Value = 1 }, Name = "VOID" }) },
