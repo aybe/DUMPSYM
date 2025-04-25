ccbdddc49c0a536fef3b7dd690c74f3cd44b0f9b
aybe <aybe@users.noreply.github.com>
2025-04-25 10:07:07 +0200

    Create UnitTestHeader.cs

---
+++ DUMPSYM.Tests/UnitTestHeader.cs
@@ -0,0 +1,36 @@
+﻿using DUMPSYM.Tests.WorkInProgress;
+
+namespace DUMPSYM.Tests;
+
+[TestClass]
+public sealed class UnitTestHeader : UnitTestBase
+{
+    [TestMethod]
+    [DynamicData(nameof(TestTopologicalSortData))]
+    public void TestTopologicalSort(Header header)
+    {
+        var b = Sorting.TryGetTopologicalSort(header, s => s, out var result);
+
+        Assert.IsTrue(b);
+
+        foreach (var value in result)
+        {
+            WriteLine(value);
+        }
+    }
+
+    public static IEnumerable<object[]> TestTopologicalSortData()
+    {
+        yield return [Headers.TYPES];
+        yield return [Headers.KERNEL];
+        yield return [Headers.LIBCD];
+        yield return [Headers.LIBDS];
+        yield return [Headers.LIBGPU];
+        yield return [Headers.LIBGTE];
+        yield return [Headers.LIBGS];
+        yield return [Headers.LIBHMD];
+        yield return [Headers.LIBMCRD];
+        yield return [Headers.LIBSND];
+        yield return [Headers.LIBSPU];
+    }
+}
