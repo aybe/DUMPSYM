c9df3b00272877c17a57744a4b8ff6e643233cc1
aybe <aybe@users.noreply.github.com>
2025-04-28 11:52:54 +0200

    Test that names aren't sorted by their address

---
+++ DUMPSYM.Tests/UnitTestXYZ.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DUMPSYM.Tests;
+
+[TestClass]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public sealed class UnitTestXYZ : UnitTestBase
+{
+    [TestMethod]
+    public void TestNamesNotSortedByAddress()
+    {
+        var check = Check(Sample.Default.Records.Where(s => s.Value is ISymbolVariable).Select(s => s.Key));
+
+        Assert.IsFalse(check); // expected, it's garbage
+
+        return;
+
+        static bool Check(IEnumerable<SymbolHeader> headers)
+        {
+            var last = -1L;
+
+            foreach (var header in headers)
+            {
+                var address = header.Address;
+
+                if (address <= last)
+                {
+                    return false;
+                }
+
+                last = address;
+            }
+
+            return true;
+        }
+    }
+}
