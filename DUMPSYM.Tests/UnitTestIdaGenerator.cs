0bf44c034352f2018eaf1dcdf21b829fbc35dfe0
aybe <aybe@users.noreply.github.com>
2025-06-04 19:26:53 +0200

    Merge generator tests

--- DUMPSYM.Tests/UnitTestIdaGenerator.cs
+++ DUMPSYM.Tests/UnitTestIdaGenerator.cs
@@ -7,10 +7,10 @@ using DUMPSYM.Generators;
 namespace DUMPSYM.Tests;
 
 [TestClass]
-public sealed class UnitTestY : UnitTestBase
+public sealed class UnitTestIdaGenerator : UnitTestBase
 {
     [TestMethod]
-    public void TestMethodY()
+    public void TestHeaderGenerator()
     {
         var options = new IdaHeaderGeneratorOptions
         {
@@ -44,5 +44,19 @@ public sealed class UnitTestY : UnitTestBase
         var hash = Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(contents)));
 
         Assert.AreEqual("a7cdae4d1fe81d23953e77bce5614ca4cde7c03128af8a437087d83c0cc4d523", hash, true);
+    }
+
+    [TestMethod]
+    public void TestScriptGenerator()
+    {
+        var generator = new IdaScriptGenerator();
+
+        var contents = generator.Generate(Sample.Default);
+
+        WriteLine(contents);
+
+        var hash = Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(contents)));
+
+        Assert.AreEqual("0023f3ddce4faa080116cb2db0c8f1dd68db0beb1f14914bd7f80228eb2ccb26", hash, true);
     }
 }
