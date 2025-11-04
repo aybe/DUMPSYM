639903988c64f65e4b8666d48976eccbfd34794d
aybe <aybe@users.noreply.github.com>
2025-11-04 22:33:57 +0100

    Upgrade DynamicData to JSON file

--- DUMPSYM.Tests/UnitTestIdaGenerator.cs
+++ DUMPSYM.Tests/UnitTestIdaGenerator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using DUMPSYM.Generators;
 using DUMPSYM.Symbols;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 
 // ReSharper disable StringLiteralTypo
 
@@ -58,14 +59,16 @@ public sealed class UnitTestIdaGenerator : UnitTestBase
     [TestMethod]
     [UsedImplicitly]
     [DynamicData(nameof(TestHeaderGeneratorData), DynamicDataDisplayName = nameof(TestHeaderGeneratorName))]
-    public void TestHeaderGenerator(string sourceFile, string targetDirectory)
+    public void TestHeaderGenerator(TestPair pair)
     {
-        if (!File.Exists(sourceFile))
+        var (source, target) = pair;
+
+        if (!File.Exists(source))
         {
-            throw new FileNotFoundException(null, sourceFile);
+            throw new FileNotFoundException(null, source);
         }
 
-        var generator = GetGenerator(sourceFile);
+        var generator = GetGenerator(source);
 
         using var headerGenerator = new IdaHeaderGenerator(generator);
 
@@ -73,13 +76,13 @@ public sealed class UnitTestIdaGenerator : UnitTestBase
 
         WriteLine(generate);
 
-        Directory.CreateDirectory(targetDirectory);
+        Directory.CreateDirectory(target);
 
-        var path = Path.Combine(targetDirectory, Path.ChangeExtension(Path.GetFileNameWithoutExtension(sourceFile), ".H"));
+        var path = Path.Combine(target, Path.ChangeExtension(Path.GetFileNameWithoutExtension(source), ".H"));
 
         File.WriteAllText(path, generate);
 
-        switch (Path.GetFileName(sourceFile)) // TODO others
+        switch (Path.GetFileName(source)) // TODO others
         {
             case "MAIN.SYM":
                 Validate(generate, "a7cdae4d1fe81d23953e77bce5614ca4cde7c03128af8a437087d83c0cc4d523");
@@ -89,26 +92,21 @@ public sealed class UnitTestIdaGenerator : UnitTestBase
 
     public static string TestHeaderGeneratorName(MethodInfo methodInfo, object[] data)
     {
-        return $"{methodInfo.Name}(\"{Path.GetFileName(data[0] as string)}\")";
+        return $"{methodInfo.Name}(\"{Path.GetFileName(((TestPair)data[0]).Source)}\")";
     }
 
-    public static IEnumerable<object[]> TestHeaderGeneratorData() // TODO use JSON file
+    public static IEnumerable<object[]> TestHeaderGeneratorData()
     {
-        return
-        [
-            [
-                @"C:\GitHub\DUMPSYM\Tests\Source\Destruction Derby (Japan)\DEMOLISH.SYM",
-                @"C:\GitHub\DUMPSYM\Tests\Target\Destruction Derby (Japan)",
-            ],
-            [
-                @"C:\GitHub\DUMPSYM\Tests\Source\Hi-Octane (Europe) (En,Fr,De,Es)\MAIN.SYM",
-                @"C:\GitHub\DUMPSYM\Tests\Target\Hi-Octane (Europe) (En,Fr,De,Es)",
-            ],
-            [
-                @"C:\GitHub\DUMPSYM\Tests\Source\Wipeout XL (USA) (Beta)\NTSC.SYM",
-                @"C:\GitHub\DUMPSYM\Tests\Target\Wipeout XL (USA) (Beta)",
-            ],
-        ];
+        var path = Path.Combine(Solution.Directory, "TestData", "test-ida-header-generator.json");
+
+        var text = File.ReadAllText(path);
+
+        var data = JsonConvert.DeserializeObject<TestPair[]>(text)!;
+
+        foreach (var pair in data)
+        {
+            yield return [pair];
+        }
     }
 
     [TestMethod]
@@ -185,5 +183,19 @@ public sealed class UnitTestIdaGenerator : UnitTestBase
         var path = Path.Combine(IdaOutputDirectory, "dumpsym_function_prototypes.py");
 
         File.WriteAllText(path, prototypes);
+    }
+
+    [PublicAPI]
+    public sealed record TestPair
+    {
+        public required string Source { get; set; }
+
+        public required string Target { get; set; }
+
+        public void Deconstruct(out string source, out string target)
+        {
+            source = Source;
+            target = Target;
+        }
     }
 }
