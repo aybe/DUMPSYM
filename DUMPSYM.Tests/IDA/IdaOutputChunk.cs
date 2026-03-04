26b57a0b34ad631807c831171148515442e92112
aybe <aybe@users.noreply.github.com>
2026-03-04 04:16:09 +0100

    Move IDA types to subfolder

---
+++ DUMPSYM.Tests/IDA/IdaOutputChunk.cs
@@ -0,0 +1,3 @@
+﻿namespace DUMPSYM.Tests.IDA;
+
+public sealed record IdaOutputChunk(Range Lines, string Value);
---
+++ DUMPSYM.Tests/IDA/IdaOutputFunction.cs
@@ -0,0 +1,9 @@
+﻿namespace DUMPSYM.Tests.IDA;
+
+public sealed record IdaOutputFunction(string Name, IdaOutputChunk Code, IdaOutputChunk Comments)
+{
+    public override string ToString()
+    {
+        return Name;
+    }
+}
