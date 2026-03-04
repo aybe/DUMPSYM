7fb5c43988c02a14cb61ba0cfb7c45984ec71399
aybe <aybe@users.noreply.github.com>
2026-03-04 02:33:47 +0100

    Convert to explicit program class

--- DUMPSYM/Program.cs
+++ DUMPSYM/Program.cs
@@ -1,40 +1,48 @@
 ﻿using DUMPSYM.Symbols;
 
-if (args.Length != 1)
+namespace DUMPSYM;
+
+internal static class Program
 {
-    Console.WriteLine("""
-                      dumpsym 2.02 (c) 1997 SN Systems Software Ltd
-                      Usage: dumpsym sym_file
-                      """);
+    public static int Main(string[] args)
+    {
+        if (args.Length != 1)
+        {    
+            Console.WriteLine("""
+                              dumpsym 2.02 (c) 1997 SN Systems Software Ltd
+                              Usage: dumpsym sym_file
+                              """);
 
-    return 1;
-}
+            return 1;
+        }
 
-var path = args[0];
+        var path = args[0];
 
-if (!File.Exists(path))
-{
-    Console.WriteLine($"Error: Can't open file '{path}' for input");
+        if (!File.Exists(path))
+        {
+            Console.WriteLine($"Error: Can't open file '{path}' for input");
 
-    return 1;
-}
+            return 1;
+        }
 
-try
-{
-    using var stream = File.OpenRead(path);
+        try
+        {
+            using var stream = File.OpenRead(path);
 
-    var file = SymbolFile.Dump(stream);
+            var file = SymbolFile.Dump(stream);
 
-    var text = file.ToString();
+            var text = file.ToString();
 
-    Console.WriteLine(text);
+            Console.WriteLine(text);
 
-    return 0;
-}
-catch (Exception e)
-{
-    Console.WriteLine("Failed to parse .SYM file:");
-    Console.WriteLine(e);
+            return 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed to parse .SYM file:");
+            Console.WriteLine(e);
 
-    return 1;
+            return 1;
+        }
+    }
 }
