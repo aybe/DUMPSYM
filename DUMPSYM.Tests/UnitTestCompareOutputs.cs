c9163cdd515d3eea62cd24ef8756a6a98a4459d8
aybe <aybe@users.noreply.github.com>
2026-03-04 02:52:33 +0100

    Clean up output comparing a bit

--- DUMPSYM.Tests/UnitTestCompareOutputs.cs
+++ DUMPSYM.Tests/UnitTestCompareOutputs.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using DUMPSYM.Symbols;
 
 namespace DUMPSYM.Tests;
@@ -10,49 +9,57 @@ public sealed class UnitTestCompareOutputs : UnitTestBase
     [DataRow(@"C:\GitHub\DUMPSYM\MAIN.SYM")]
     public void TestCompareOutputs(string path)
     {
-        string source;
+        var source = GetSourceText(path);
 
-        using (var consoleCapture = new ConsoleCapture())
-        {
-            consoleCapture.OutputToSource = false;
-            consoleCapture.OutputToTarget = true;
-
-            Original.__main(["", path]);
-
-            source = consoleCapture.ToString();
-        }
-
-        using var stream = File.OpenRead(path);
-
-        var file = SymbolFile.Dump(stream);
-
-        var target = file.ToString();
+        var target = GetTargetText(path);
 
         CompareLines(source, target);
     }
 
-    private void CompareLines(string source, string target)
+    private static string GetSourceText(string path)
+    {
+        using var writer = new StringWriter();
+
+        var previous = Console.Out;
+
+        Console.SetOut(writer);
+
+        Original.__main(["", path]);
+
+        Console.SetOut(previous);
+
+        var text = writer.ToString();
+
+        return text;
+    }
+
+    private static string GetTargetText(string path)
+    {
+        using var stream = File.OpenRead(path);
+
+        var file = SymbolFile.Dump(stream);
+
+        var text = file.ToString();
+
+        return text;
+    }
+
+    private void CompareLines(string sourceText, string targetText)
     {
         var separator = new[] { "\r\n", "\r", "\n" };
 
         const StringSplitOptions options = StringSplitOptions.None;
 
-        var sourceLines = source.Split(separator, options);
-        var targetLines = target.Split(separator, options);
+        var sourceLines = sourceText.Split(separator, options);
+        var targetLines = targetText.Split(separator, options);
 
-        var sourceCount = sourceLines.Length;
-        var targetCount = targetLines.Length;
-
-        WriteLine($"Source lines: {sourceCount}");
-        WriteLine($"Target lines: {targetCount}");
-
-        Assert.AreEqual(sourceCount, targetCount, "Lines count do not match.");
+        Assert.HasCount(sourceLines.Length, targetLines, "Lines count don't match.");
 
         var line = 0;
 
         var list = new List<(int LineIndex, string SourceLine, string TargetLine)>();
 
-        for (var i = 0; i < sourceCount; i++)
+        for (var i = 0; i < sourceLines.Length; i++)
         {
             var sourceLine = sourceLines[i];
             var targetLine = targetLines[i];
@@ -68,75 +75,16 @@ public sealed class UnitTestCompareOutputs : UnitTestBase
         foreach (var (lineIndex, sourceLine, targetLine) in list)
         {
             WriteLine(
-                $"Wrong match @ {lineIndex}:\n" +
+                $"Line {lineIndex} doesn't match:\n" +
                 $"\tSource: \"{sourceLine}\"\n" +
                 $"\tTarget: \"{targetLine}\"");
         }
 
-        var nPass = sourceCount - list.Count;
-        var nFail = list.Count;
-        var count = 1.0d / sourceCount;
+        var pass = sourceLines.Length - list.Count;
+        var fail = list.Count;
 
-        Assert.AreEqual(0, nFail, $"PASS = {nPass} ({count * nPass:P}), FAIL = {nFail} ({count * nFail:P})");
-    }
-
-    private sealed class ConsoleCapture : TextWriter
-    {
-        public ConsoleCapture()
-        {
-            Source = Console.Out;
-
-            Target = new StringWriter();
-
-            Console.SetOut(this);
-        }
-
-        private TextWriter Source { get; }
-
-        private StringWriter Target { get; }
-
-        public override Encoding Encoding => Encoding.UTF8;
-
-        public bool OutputToSource { get; set; } = true;
-
-        public bool OutputToTarget { get; set; } = true;
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            Console.SetOut(Source);
-        }
-
-        public override void Flush()
-        {
-            base.Flush();
-
-            Source.Flush();
-
-            Target.Flush();
-        }
-
-        public override void Write(char value)
-        {
-            if (OutputToSource)
-            {
-                Source.Write(value);
-            }
-
-            if (OutputToTarget)
-            {
-                Target.Write(value);
-            }
-        }
-
-        public override string ToString()
-        {
-            Flush();
-
-            var s = Target.ToString();
-
-            return s;
-        }
+        Assert.AreEqual(0, fail,
+            $"PASS = {pass} ({(double)pass / sourceLines.Length:P}), " +
+            $"FAIL = {fail} ({(double)fail / sourceLines.Length:P})");
     }
 }
