3a6dcb60610dcd214b397751a96ede8947b09494
aybe <aybe@users.noreply.github.com>
2026-03-04 04:38:33 +0100

    TestIdaOutputSplit is totally useless

--- DUMPSYM.Tests/IDA/IdaOutputChunk.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace DUMPSYM.Tests.IDA;
-
-public sealed record IdaOutputChunk(Range Lines, string Value);
--- DUMPSYM.Tests/IDA/IdaOutputFunction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DUMPSYM.Tests.IDA;
-
-public sealed record IdaOutputFunction(string Name, IdaOutputChunk Code, IdaOutputChunk Comments)
-{
-    public override string ToString()
-    {
-        return Name;
-    }
-}
